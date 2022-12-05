4b6a4879b7e8cfcb2202eb2ac706e047ac385131
Lucas Felipe Costa <lucasfelipecost4@gmail.com>
2022-12-04 21:59:28 -0300

    Testando gerar horios

--- preencher.py
+++ preencher.py
@@ -36,7 +36,68 @@ def abrirJiraLoga():
 def abrirExcel():
     newTabela = []
     tabela = pd.read_excel('jira1.xlsx')
-   
+    for i, linha in enumerate(tabela["linha"]):
+        created = tabela.loc[i, "Created"]
+        issue = tabela.loc[i, "Issue"]
+        str_date = created.strftime("%d/%b/%Y")
+        str_hora = created.strftime("%H:%M:%S")
+        if checkKey(newTabela, issue, str_date):
+            for i in range(len(newTabela)):
+                value = newTabela[i]
+                if issue == value.get('issue') and value.get('str_date') == str_date:
+                    t1 = datetime.strptime(value.get('str_hora'), "%H:%M:%S")
+                    t2 = datetime.strptime(str_hora, "%H:%M:%S")
+                    dif = t2-t1
+                    time = dif.total_seconds()
+                    if value.get('time'):
+                        time += value.get('time')
+
+                    newTabela[i] = {**value, 'time': time}
+        else:
+            newTabela.append({
+                'created': created,
+                'str_date': str_date,
+                'issue': issue,
+                'str_hora': str_hora,
+                'time': 0.0
+            })
+    
+    
+    countHora = []
+    for i in range(len(newTabela)):
+        value = newTabela[i]
+        time = value.get('time')
+        if time <= 1800:
+            time = 1800
+        data = value.get('str_date')
+        cont = time
+
+        def checkKey2(arr, str_date):
+            if len(arr) < 1:
+                return False
+            for dic in arr:
+                if dic.get('str_date') == str_date:
+                    return True
+            return False
+        if checkKey2(countHora, data):
+            for i in range(len(countHora)):
+                value2 = countHora[i]
+                if value2.get('str_date') == data:
+                    cont += value2.get('time')
+                    countHora[i] = {**value2, 'time': cont}
+        else:
+            countHora.append({
+                'str_date': data,
+                'time': cont
+            })
+
+        # hours = "%dh %dm" % hm_from_seconds(time)
+        # newTabela[i] = {**value, 'time2': time, 'hours': hours}
+        # print(newTabela[i], end='\n')
+    for i in range(len(countHora)):
+        value = countHora[i]
+        hours = "%dh %dm" % hm_from_seconds(value.get('time'))
+        print({'str_date': value.get('str_date'), 'hours': hours}, end='\n')
 # abrir registro
 
 
